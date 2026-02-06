dd4c868d94566d8cc24eefff47a1cb12e818311d
jontirabassi <jonathontirabassi@gmail.com>
2026-02-06 17:29:47 -0500

    testing app.get

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, "public")))  // if the browser asks for a file, look in this directory
 
 app.get('/', (req, res) => { // load index.html when path is /
-  res.sendFile(path.join(__dirname, "public", 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/path',  (req, res) => { // load index.html when path is /path
