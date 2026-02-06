81333275bd8bdac5cfcef73220cef6f86d8461e2
jontirabassi <jonathontirabassi@gmail.com>
2026-02-06 17:31:42 -0500

    added 'public' tp app.get in server.js

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, "public")))  // if the browser asks for a file, look in this directory
 
 app.get('/', (req, res) => { // load index.html when path is /
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/path',  (req, res) => { // load index.html when path is /path
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
