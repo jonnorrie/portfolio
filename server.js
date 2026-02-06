8e22da9ea61c01311685f5555073ae4cd9571cf4
jontirabassi <jonathontirabassi@gmail.com>
2026-02-06 17:27:36 -0500

    moved index.html to public folder. added /path route

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, "public")))  // if the browser asks for a file, look in this directory
+
+app.get('/', (req, res) => { // load index.html when path is /
+  res.sendFile(path.join(__dirname, "public", 'index.html'));
+});
+
+app.get('/path',  (req, res) => { // load index.html when path is /path
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
