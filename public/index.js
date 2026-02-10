2a2031953a9160c8bc4d58b4a98357bdc1ee1f78
jontirabassi <jonathontirabassi@gmail.com>
2026-02-10 12:22:56 -0500

    same as previous commit

--- public/index.js
+++ public/index.js
@@ -1,5 +1,35 @@
-let submits = document.querySelector('.submit')
+let lists_div = document.querySelector('.lists')
 
-submits.addEventListener('click', event => {
-  event.preventDefault()
+async function loadList() {
+  let response = await fetch('http://localhost:3000/items')
+  let items = await response.json();
+
+  items.forEach(item => {
+    let li = document.createElement("li")
+    let p = document.createElement("p")
+
+    li.appendChild(p)
+    p.textContent = `${item.name}`
+    lists_div.children[0].children[1].appendChild(li)
+    //console.log(item.name)
+  })
+
+  //console.log(items[0].name)
+}
+
+loadList()
+
+lists_div.addEventListener('click', event => {
+
+  if (event.target.tagName === 'INPUT') {
+    event.preventDefault()
+
+    // make request to local postgres database for all items
+    //getFromPostgres()
+
+    // populate the list with retrieved items
+    //loadList()
+
+    console.log(`${event.target.parentNode.parentNode.id} submit button clicked`)
+  }
 })
