c4b14b7f91905f337b3eac551b5db0d4c8d5faeb
jonnorrie <jonathontirabassi@gmail.com>
2026-06-16 16:13:29 -0400

    Added styling and filler content to About and Projects components

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,22 @@
+const projects = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+]
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div className="projects-page">
+      <h1>Projects</h1>
+      <div className="projects-grid">
+        {projects.map(p => (
+          <div key={p.id} className="project-box"></div>
+        ))}
+      </div>
+    </div>
   )
 }
 
